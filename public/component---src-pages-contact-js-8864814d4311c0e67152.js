54b1c7db195ad65def99e7992c7cd73f0bd62fea
Admin <albert@gotitapp.co>
2020-06-23 10:19:17 +0700

    gh page

---
+++ public/component---src-pages-contact-js-8864814d4311c0e67152.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"Cuy+":function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),u=t.n(a),l=t("Bl7J");e.default=function(){return u.a.createElement(l.a,null,u.a.createElement("h1",null,"Liên hệ"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-8864814d4311c0e67152.js.map
